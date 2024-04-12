1becc1d632ae91376e0d8511afcfbe229211a777
Salik Azeem <118605641+salikAzeem@users.noreply.github.com>
2024-04-13 03:22:48 +0530

    login.js

--- login.js
+++ login.js
@@ -6,8 +6,20 @@ mongoose.connect("mongodb://localhost:27017/")
 .catch(()=>{
        console.log('error');
 })
-const username = document.getElementById("username");
-const password = document.getElementById("password");
+// const username = document.getElementById("username");
+// const password = document.getElementById("password");
+const app=require('./app');
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 4000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
 
 function login() {
   // Ketika Username dan Password Benar
