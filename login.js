9e8f461f9f9dc8425e02c2987cb98f22cfb1fe45
Salik Azeem <118605641+salikAzeem@users.noreply.github.com>
2024-04-13 16:02:18 +0530

    login.js

--- login.js
+++ login.js
@@ -14,7 +14,7 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 4000;
 app.get('/', (req, res) => {
-  res.type('html').send(html));
+  res.type('html').send(html);
 })
 
 app.listen(port, () => {
